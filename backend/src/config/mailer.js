8646e14286f5e2f4cd841773f2eea586af949ebe
Nandeesha <your-email@gmail.com>
2026-08-26 12:28:52 +0530

    vercerl-json

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -2,10 +2,15 @@ const nodemailer = require("nodemailer");
 
 // Create Gmail transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
